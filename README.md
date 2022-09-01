f61dc61c0a303f1074f345f53ab3b4bcc00f0334
oxyeez <marcelinliehn@gmail.com>
2022-09-01 17:55:42 +0200

    ✨ Upgrade download quality to 1080p

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Youtube Video Downloader RayCast Extension
 
-##### V0.1
+##### V0.2
 
 Basic extension to downloads Youtube videos in .mp4 format.  
-Videos are downloaded in a default quality which is 360p, and by default in the Downloads folder.  
+Videos are downloaded in 1080p by default, and by default in the Downloads folder.  
 I'll add in the future some more options.
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import { Form, ActionPanel, showToast, Toast, Icon, Action } from "@raycast/api";
-import ytdl from 'ytdl-core';
-import fs from 'fs';
-import os from 'os';
-import isValidFilename from 'valid-filename';
+import ytdl from "ytdl-core";
+import { createWriteStream, chmodSync } from "fs";
+import { homedir } from "os";
+import isValidFilename from "valid-filename";
+var ffmpegPath = require("ffmpeg-static") as string;
+import cp from "child_process";
 
 interface FormInput {
   url: string;
@@ -55,15 +57,73 @@ function DownloadAction() {
       });
       return;
     }
-    var home = os.homedir();
-    var filepath = home.concat('/Downloads/', name, '.mp4');
-    ytdl(url).pipe(fs.createWriteStream(filepath));
+
+    var home = homedir();
+    var filepath = home.concat("/Downloads/" + name + ".mp4");
+    let ffmpegProcess = cp.spawn(
+      // "/Users/marcelinliehn/.config/raycast/extensions/youtube-downloader/node_modules/ffmpeg-static/ffmpeg",
+      ffmpegPath.replace("ffmpeg", "node_modules/ffmpeg-static/ffmpeg"),
+      [
+        // supress non-crucial messages
+        "-loglevel",
+        "8",
+        "-hide_banner",
+        // input audio and video by pipe
+        "-i",
+        "pipe:3",
+        "-i",
+        "pipe:4",
+        // map audio and video correspondingly
+        "-map",
+        "0:a",
+        "-map",
+        "1:v",
+        // no need to change the codec
+        "-c",
+        "copy",
+        // output mp4 and pipe
+        "-f",
+        "matroska",
+        "pipe:5",
+      ],
+      {
+        // no popup window for Windows users
+        windowsHide: true,
+        stdio: [
+          // silence stdin/out, forward stderr,
+          "inherit",
+          "inherit",
+          "inherit",
+          // and pipe audio, video, output
+          "pipe",
+          "pipe",
+          "pipe",
+        ],
+      }
+    );
+
+    showToast({
+      style: Toast.Style.Animated,
+      title: 'Downloading...',
+    });
+
+    let vid = ytdl(url, { filter: (format) => format.itag === 137 });
+    let aud = ytdl(url, { filter: (format) => format.itag === 140 });
+
+    aud.pipe(ffmpegProcess.stdio[3] as NodeJS.WritableStream);
+    vid.pipe(ffmpegProcess.stdio[4] as NodeJS.WritableStream);
+
+    var stream = ffmpegProcess.stdio[5]!.pipe(createWriteStream(filepath));
+    stream.on('finish', () => {
+      showToast({
+        style: Toast.Style.Success,
+        title: 'Downloaded',
+      })
+    });   
   }
-  return (
-    <Action.SubmitForm
-      icon={Icon.Checkmark}
-      title="Format"
-      onSubmit={download}
-    />
-  );
+
+  
+  return <Action.SubmitForm icon={Icon.Checkmark} title="Format" onSubmit={download} />;
 }
+
+
